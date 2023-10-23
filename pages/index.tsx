79c441e2f3ca25781bf3003298afa0c20e88ec83
Martin David <martin.david@applydigital.coml>
2023-10-23 15:39:46 -0300

    feat: add navbar

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,13 @@
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
+import Navbar from '@/components/Navbar/Navbar'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home(): JSX.Element {
   return (
     <main>
+      <Navbar />
       <h1>Avo Store</h1>
     </main>
   )
